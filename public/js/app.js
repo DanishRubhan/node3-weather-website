49b94e3b61daec59111c0cddf17c3b0504cfd804
Your Name <you@example.com>
2022-01-10 11:36:38 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('client side server!!')
+
+const msg1 = document.getElementById('msg1')
+
+const msg2 = document.getElementById('msg2')
+
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    msg1.textContent = 'Loading...'
+    msg2.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                msg1.textContent = data.error
+            } else {
+                msg1.textContent = data.location
+                msg2.textContent = data.forecast
+            }
+        })
+    })
+})
+
